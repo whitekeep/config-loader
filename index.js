593255a38cb548a3d9fdfaf72fee9976a2657b0a
whitekeep <vip.fox.fox.3@gmail.com>
2021-11-12 15:36:13 +0300

    fix final

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Ajv = require('ajv');
 const ajv = new Ajv();
+const path = require('path');
 
 function loadConfig(config_folder) {
   // стандартный конфиг
   try {
-    var config_default = require(`${config_folder}config_default.json`);
-    var validate = ajv.compile(require(`${config_folder}config_schema.json`));
+    def_path = path.join('../../', config_folder, 'config_default.json');
+    sch_path = path.join('../../', config_folder, 'config_schema.json');
+    var config_default = require(def_path);
+    var validate = ajv.compile(require(sch_path));
     if (config_default.config_validate == true) {
       if (!validate(config_default)) {
         console.log(validate.errors);
@@ -18,11 +21,29 @@ function loadConfig(config_folder) {
   }
 
   // подключаем логгер loadConfig
+  conf_logger = {
+    info: {
+      console_output: true,
+      file_write: false,
+      file_path: '../path/to/file.log',
+    },
+    error: {
+      console_output: true,
+      file_write: false,
+      file_path: '../path/to/file.log',
+    },
+    access: {
+      console_output: false,
+      file_write: false,
+      file_path: '../path/to/file.log',
+    },
+  };
   const Logger = require('log-my-ass');
-  const log = new Logger(config_default.logger, 'ConfigLoader');
+  const log = new Logger(conf_logger, 'ConfigLoader');
 
   try {
-    var config = require(`${config_folder}/config.json`);
+    conf_path = path.join('../../', config_folder, 'config.json');
+    var config = require(conf_path);
     if (config.config_validate == true) {
       if (!validate(config)) {
         throw validate.errors;
