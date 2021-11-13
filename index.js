35722dfc791f5a70cd33047869c8d77123b50053
whitekeep <vip.fox.fox.3@gmail.com>
2021-11-13 13:46:22 +0300

    add exit when config broken

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Ajv = require('ajv');
-const ajv = new Ajv();
 const path = require('path');
+const process = require('process');
+
+const ajv = new Ajv();
 
 function loadConfig(config_folder) {
-  // стандартный конфиг
+  // check default config
   try {
     def_path = path.join('../../', config_folder, 'config_default.json');
     sch_path = path.join('../../', config_folder, 'config_schema.json');
@@ -11,16 +13,17 @@ function loadConfig(config_folder) {
     var validate = ajv.compile(require(sch_path));
     if (config_default.config_validate == true) {
       if (!validate(config_default)) {
-        console.log(validate.errors);
-        return 0;
+        throw validate.errors;
       }
     }
   } catch (err) {
+    // exit if error with loading default config
     console.log(err);
-    return;
+    console.log(`error load configuration. exit.`);
+    process.exit(1);
   }
 
-  // подключаем логгер loadConfig
+  // init logger (log-my-ass npm pckg)
   conf_logger = {
     info: {
       console_output: true,
@@ -41,23 +44,23 @@ function loadConfig(config_folder) {
   const Logger = require('log-my-ass');
   const log = new Logger(conf_logger, 'ConfigLoader');
 
+  // load normal config
   try {
     conf_path = path.join('../../', config_folder, 'config.json');
     var config = require(conf_path);
     if (config.config_validate == true) {
       if (!validate(config)) {
         throw validate.errors;
-        //todo: логгирование
       }
     }
     log.info('Конфигурация загружена');
   } catch (err) {
-    log.info('Не удается загрузить конфигурационный файл');
-    log.error(err);
+    // error with load normal config
+    log.error(err, 'Не удается загрузить конфигурационный файл');
     log.info('Загрузка конфигурации по умолчанию');
     config = config_default;
   }
-  //todo: остановка приложения при ломанном дефолтном конфиге
+
   return config;
 }
 
